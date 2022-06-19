b965ac2b6823f8ce37ebc4fd9f30ed8c767b0e8a
Pr0uxx <mikieghoward@gmail.com>
2022-06-19 13:36:55 +0100

    Add project files.

---
+++ Xcersise.Api/Controllers/ExcersiseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xcersise.Api.Models.Excersise;
+
+namespace Xcersise.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcersiseController : ControllerBase
+    {
+		[HttpGet("GetExcersises")]
+		public IEnumerable<ExcersiseType> GetExcersises()
+        {
+			return new ExcersiseType[] {
+				new()
+                {
+					Title = "Jog",
+					Description = "Train stamina and last longer on the road"
+				},
+				new() { 
+					Title = "Weight Lifting",
+					Description = "Grow stronger and more powerful"
+				},
+				new() {
+					Title = "Yoga",
+					Description = "Find peace within yourself"
+				}
+			};
+		}
+    }
+}
---
+++ Xcersise.Api/Models/Excersise/ExcersiseType.cs
@@ -0,0 +1,8 @@
+﻿namespace Xcersise.Api.Models.Excersise
+{
+    public class ExcersiseType
+    {
+        public string Title { get; set; } = "Title not set";
+        public string Description { get; set; } = "Description not set";
+    }
+}
